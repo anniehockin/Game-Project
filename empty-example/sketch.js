818cd5cfe694d2907ad2bf1b2a1d121a764b5602
Annie Hockin <anniehockin@gmail.com>
2020-03-04 11:43:30 -0800

    added new colors

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -3,10 +3,9 @@ function setup() {
 }
 
 function draw(){
-	background(220,100,3);
-  fill("black")
-  ellipse(100,100,40,50);
+	background(244,100,210);
   fill("blue")
+  ellipse(100,100,40,50);
+  fill("pink")
   rect(200,200,100,150)
-
 }
