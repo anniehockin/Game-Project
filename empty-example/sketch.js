4b92033908559ae004787a9e1a91214497f3d5e7
Annie Hockin <anniehockin@gmail.com>
2020-03-04 11:56:04 -0800

    added other shapes

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -2,10 +2,19 @@ function setup() {
   createCanvas(700, 600);
 }
 
+{
+
 function draw(){
-	background(244,100,210);
-  fill("blue")
+	background(255,255,200);
+  fill("blue");
   ellipse(100,100,40,50);
-  fill("pink")
-  rect(200,200,100,150)
+  fill("pink");
+  rect(200,200,100,150);
+  fill("orange");
+  ellipse(400,200,70);
+  fill("green")
+  rect(500,500,50,30);
+  fill("yellow");
+  ellipse(600,400,100)
+}
 }
