0ca5384aae7adce6fedd32228906acd3742c0d38
Annie Hockin <anniehockin@gmail.com>
2020-03-10 11:06:53 -0700

    Made drawing

--- 2-array-of-objects/sketch.js
+++ 2-array-of-objects/sketch.js
@@ -2,26 +2,38 @@
 //create an empty array called balls
 
 let balls = [];
+let triangles= [];
+let squares= [];
 
 function setup() {
-  createCanvas(800, 400);
+  createCanvas(900, 600);
 
 }
 
 function draw(){
-	background(220);
+	background(189,180,255,100);
 
 //	draw all the balls in that array
 	for (let i = 0; i < balls.length; i++) {
 	    balls[i].drawBall();
       balls[i].moveBall();
+      triangles[i].drawBall();
+      triangles[i].moveBall();
+      squares[i].drawBall();
+      squares[i].moveBall();
 	  }
 }
 
 function keyPressed(){ //every time you push a key, make a new ball from the ball class and add it to the balls array
   let  b = new Ball(100, 100);
+  let c = new Triangle(300,300);
+  let d= new Square(500,200)
   balls.push(b);
   console.log(balls);
+  triangles.push(c);
+  console.log(triangles);
+  squares.push(d);
+  console.log(squares);
 }
 
 //ball class from which to create new balls with similar properties.
@@ -34,14 +46,63 @@ class Ball {
 
 	drawBall(){  // draw a ball on the screen at x,y
     		stroke(0);
-    		fill("red");
-		    ellipse(this.x,this.y,10,10);
+    		fill("blue");
+		    ellipse(this.x,this.y,40,40);
 	}
 
 	moveBall(){ //update the location of the ball, so it moves across the screen
 		this.x = this.x+2;
 		this.y = this.y+.5;
-	}
+	}}
+
+  class Triangle {
+
+  	constructor(x,y){ //every ball needs an x value and a y value
+  		    this.x = x;
+      		this.y = y;
+
+          // if (this.x===800) {
+          //   this.speed-this.speed;
+          // }
+          // if (this.x===400) {
+          //   this.speed-this.speed;
+          // }
+          }
 
 
+  	drawBall(){  // draw a ball on the screen at x,y
+      		stroke(0);
+      		fill("purple");
+  		    ellipse(this.x,this.y,10,10);
+  	}
+
+  	moveBall(){ //update the location of the ball, so it moves across the screen
+  		this.x = this.x+10;
+  		this.y = this.y+30;
+
+}}
+class Square {
+
+  constructor(x,y,speed){ //every ball needs an x value and a y value
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+
+}
+
+  drawBall(){  // draw a ball on the screen at x,y
+        stroke(8);
+        fill("yellow");
+        ellipse(this.x,this.y,50,25);
+  }
+
+  moveBall(){ //update the location of the ball, so it moves across the screen
+    if (this.x>300) {
+      this.speed=-this.speed;
+    }
+
+
+    this.x = this.x+this.speed;
+    this.y = this.y+4;
+}
 }
--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,20 +1,7 @@
-function setup() {
-  createCanvas(700, 600);
-}
+class Ball {
 
-{
-
-function draw(){
-	background(255,255,200);
-  fill("blue");
-  ellipse(100,100,40,50);
-  fill("pink");
-  rect(200,200,100,150);
-  fill("orange");
-  ellipse(400,200,70);
-  fill("green")
-  rect(500,500,50,30);
-  fill("yellow");
-  ellipse(600,400,100)
-}
+ constructor(x,y){
+		this.x = x;
+    		this.y = y;
+          }
 }
