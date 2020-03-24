a996c11bd24f1f4169d4513068c5a04cf8ffbd5f
Annie Hockin <anniehockin@gmail.com>
2020-03-24 13:32:21 -0700

    finish

--- 4-sound /sketch.js
+++ 4-sound /sketch.js
@@ -7,8 +7,8 @@ let me;
 let mySound;
 
 function preload() {
-  soundFormats('mp3', 'ogg');
-  mySound = loadSound('boing1.mp3');
+  soundFormats('wav', 'ogg');
+  mySound = loadSound('gongsound.wav');
 }
 
 function setup() {
@@ -20,7 +20,7 @@ function setup() {
 }
 
 function draw(){
-	background(220);
+	background(236,226,255,100);
 
   me.drawMe();
   me.moveMe();
@@ -49,17 +49,24 @@ class Avatar {
         this.speed = speed;
 	}
 
-	drawMe(){  // draw the running person
-    		stroke("green");
-        strokeWeight(3);
-    		fill("blue");
-		    ellipse(this.x,this.y,20,20);
-        line(this.x,this.y, this.x, this.y+40);
-        line(this.x, this.y+40, this.x-20, this.y+60);
-        line(this.x, this.y+40, this.x+10, this.y+50);
-        line(this.x+10, this.y+50, this.x+5, this.y+60);
-        line(this.x, this.y+15, this.x-10, this.y+25);
-        line(this.x-10, this.y+25, this.x+10, this.y+35);
+	drawMe(){  // draw the snowman
+    		stroke("white");
+        strokeWeight(2);
+    		fill("white");
+		    ellipse(this.x,this.y-7,30,30);
+        ellipse(this.x,this.y+28,40,40);
+        ellipse(this.x,this.y+73,50,50);
+        fill("black");
+        ellipse(this.x-4,this.y-9,5,5);
+        ellipse(this.x+4,this.y-9,5,5);
+        ellipse(this.x,this.y+30,5,5);
+        ellipse(this.x,this.y+75,5,5);
+        line(this.x-20,this.y+30,2,2,2,2);
+        line(this.x+20,this.y+30,500,2);
+        fill("brown");
+        stroke("brown");
+        rect(this.x-10,this.y-43,20,20);
+
 	}
 
 	moveMe(){
@@ -85,16 +92,16 @@ class Ball {
 	//every ball needs an x value, a y value, and a speed
 	constructor(x,y, speed){
 		this.x = x;
-    		this.y = y;
-        	this.speed = speed;
+    this.y = y;
+    this.speed = speed;
 	}
 
 	// draw a ball on the screen at x,y
 	drawBall(){
-    		stroke(0);
-        	strokeWeight(1);
-    		fill("red");
-		ellipse(this.x,this.y,10,10);
+    	stroke(157,237,255,100);
+      strokeWeight(1);
+    	fill("white");
+		  ellipse(this.x,this.y,10,10);
 	}
 
 	//update the location of the ball, so it moves across the screen
@@ -103,13 +110,13 @@ class Ball {
 		this.y = this.y+.5;
 	}
 
-	//if the ball hits the paddle, change the speed value to negative (send it in the opposite direction)
+	//if the ball hits the person, change the speed value to negative (send it in the opposite direction)
   	bounceBall(){
-    		if (this.x >= me.x-15 && this.x <= me.x+15 && this.y > me.y-40 && this.y < me.y+40){
+    		if (this.x >= me.x-30 && this.x <= me.x+15 && this.y > me.y-40 && this.y < me.y+100){
       			this.speed = -this.speed;
             mySound.setVolume(0.1);
             mySound.play();
-    		}
-  	}
+          }
+        }
 
 }
